1e6d2b83188fdac01c1b1df8d2d3c4b182bad62f
Ryan Knight <ryan@grandcloud.com>
2019-11-01 10:07:19 -0600

    fixing home page

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 
 [Websocket Code Adopted from this blog post](https://developer.okta.com/blog/2018/09/24/reactive-apis-with-spring-webflux)
 
-
+export BOOTSTRAP_SERVER=localhost:9092
+export KAFKA_TOPIC=stackoverflow-questions
 
 ./mvnw spring-boot:run
--- src/main/java/portable/DemoApp.java
+++ src/main/java/portable/DemoApp.java
@@ -1,15 +1,33 @@
 package portable;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.Disposable;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 @Slf4j
 public class DemoApp {
+
+    //This loads the index.html when you hit / to force a default homepage
+    @Bean
+    public RouterFunction<ServerResponse> htmlRouter(
+            @Value("classpath:index.html") Resource html) {
+        return route(GET("/"), request
+                -> ok().contentType(MediaType.TEXT_HTML).syncBody(html)
+        );
+    }
 
     @Bean
     ApplicationRunner appRunner(KafkaConsumer kafkaConsumer) {
